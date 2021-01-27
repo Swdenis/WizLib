2ba03385f398142a0aef0faa329a34fd6ab196e0
Swdenis <swdenis@mail.ru>
2021-01-27 17:36:49 +0100

    * .gitignore: Initial commit
    * WizLib.sln:
    * Program.cs:
    * Startup.cs:
    * WizLib.csproj:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Category.cs:
    * Privacy.cshtml:
    * _ViewImports.cshtml:
    * WizLib_Models.csproj:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * launchSettings.json:
    * LICENSE.txt:
    * ErrorViewModel.cs:
    * jquery.js:
    * WizLib_DataAccess.csproj:
    * jquery.min.js:
    * jquery.min.map:
    * ApplicationDbContext.cs:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.min.css:
    * bootstrap.css.map:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap.bundle.js.map:
    * jquery.validate.js:
    * bootstrap.bundle.min.js:
    * LICENSE.txt:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * ApplicationDbContextModelSnapshot.cs:
    * bootstrap-reboot.min.css.map:
    * 20210127162650_AddCategoryTableToDb.cs:
    * 20210127162937_AddCategoryTableToDb.cs:
    * additional-methods.min.js:
    * 20210127162650_AddCategoryTableToDb.Designer.cs:
    * 20210127162937_AddCategoryTableToDb.Designer.cs:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WizLib_Models.Models;
+
+namespace WizLib_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
