2b4e49ac8d2fe6779a8135417fa205e56f501d74
Swdenis <swdenis@mail.ru>
2021-02-02 18:57:51 +0100

    * Book.cs: * Upsert.cshtml: * Details.cshtml: * BookVM.cs: * WizLib_Models.csproj: * BookController.cs: * 20210202170802_BookDetailNullable.cs: * ApplicationDbContextModelSnapshot.cs: * 20210202170802_BookDetailNullable.Designer.cs:
    * Index.cshtml: Added eager loading

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WizLib_DataAccess.Data;
 using WizLib_Models.Models;
+using WizLib_Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,64 +25,122 @@ namespace WizLib.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Book> objList = _db.Books.ToList();
+            List<Book> objList = _db.Books.Include(u => u.Publisher).ToList();
+
+            //foreach(var obj in objList)
+            //{
+            //    //obj.Publisher = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == obj.Publisher_Id);
+            //    _db.Entry(obj).Reference(u => u.Publisher).Load();
+            //}
 
             return View(objList);
+
         }
 
-        //public IActionResult Upsert(int? id)
-        //{
+        public IActionResult Upsert(int? id)
+        {
 
-        //    Publisher obj = new Publisher();
+            BookVM obj = new BookVM();
 
-        //    if (id == null)
-        //    {
-        //        return View(obj);
-        //    }
+            obj.PublisherList = _db.Publishers.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Publisher_Id.ToString()
+            });
 
-        //    obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+            if (id == null)
+            {
+                return View(obj);
+            }
 
-        //    if (obj == null)
-        //    {
-        //        return NotFound();
-        //    }
+            obj.Book = _db.Books.FirstOrDefault(u => u.Book_Id == id);
 
-        //    return View(obj);
-        //}
+            if (obj == null)
+            {
+                return NotFound();
+            }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Upsert(Publisher obj)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        if (obj.Publisher_Id == 0)
-        //        {
-        //            //this is create
-        //            _db.Publishers.Add(obj);
-        //        }
+            return View(obj);
+        }
 
-        //        else
-        //        {
-        //            //this is update
-        //            _db.Publishers.Update(obj);
-        //        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(BookVM obj)
+        {
+            
+                if (obj.Book.Book_Id == 0)
+                {
+                    //this is create
+                    _db.Books.Add(obj.Book);
+                }
 
-        //        _db.SaveChanges();
-        //        return RedirectToAction(nameof(Index));
-        //    }
+                else
+                {
+                    //this is update
+                    _db.Books.Update(obj.Book);
+                }
 
-        //    return View(obj);
-        //}
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            
+        }
 
-        //public IActionResult Delete(int id)
+        public IActionResult Details(int? id)
+        {
 
-        //{
-        //    var objFromObj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
-        //    _db.Publishers.Remove(objFromObj);
-        //    _db.SaveChanges();
-        //    return RedirectToAction(nameof(Index));
+            BookVM obj = new BookVM();
 
-        //}
+            if (id == null)
+            {
+                return View(obj);
+            }
+
+            obj.Book = _db.Books.Include(u => u.BookDetail_Id).FirstOrDefault(u => u.Book_Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(BookVM obj)
+        {
+
+            if (obj.Book.BookDetail.BookDetail_Id == 0)
+            {
+                //this is create
+                _db.BookDetails.Add(obj.Book.BookDetail);
+                _db.SaveChanges();
+
+                var BookFromDb = _db.Books.FirstOrDefault(u => u.Book_Id == obj.Book.Book_Id);
+                BookFromDb.BookDetail_Id = obj.Book.BookDetail.BookDetail_Id;
+            }
+
+            else
+            {
+                //this is update
+                _db.BookDetails.Update(obj.Book.BookDetail);
+                _db.SaveChanges();
+            }
+
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        public IActionResult Delete(int id)
+
+        {
+            var objFromObj = _db.Books.FirstOrDefault(u => u.Book_Id == id);
+            _db.Books.Remove(objFromObj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
---
+++ WizLib_Models/ViewModels/BookVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WizLib_Models.Models;
+
+namespace WizLib_Models.ViewModels
+{
+    public class BookVM
+    {
+        public BookVM()
+        {
+        }
+
+        public Book Book { get; set; }
+
+        public IEnumerable<SelectListItem> PublisherList { get; set; }
+    }
+}
